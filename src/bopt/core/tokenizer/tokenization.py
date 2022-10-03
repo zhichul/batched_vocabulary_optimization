039a988a1546bc48299d6165f284fca23f3651c1
Brian Lu <blu@brians-mbp-19.acasmart.jh.edu>
2022-10-03 15:25:13 -0400

    fixed bug with padding, added special token handling, refactored duplicate code

--- src/bopt/core/tokenizer/tokenization.py
+++ src/bopt/core/tokenizer/tokenization.py
@@ -1,9 +1,10 @@
-from typing import List, Tuple
+from typing import List, Tuple, Any, Callable, TypeVar, Set
 
 import torch
 
 from bopt.core.integerize import Integerizer
 
+T = TypeVar("T", str, List[str])
 
 class TokenizationMixin:
 
@@ -12,6 +13,7 @@ class TokenizationMixin:
                  continuing_subword_prefix: str = None,
                  pad_token: str = "[PAD]",
                  max_unit_length: int = 1e9,
+                 specials: List[str] = None,
                  **kwargs):
         super().__init__(*args, **kwargs)
         self.parallel_backward_mask_cache = dict()
@@ -22,6 +24,37 @@ class TokenizationMixin:
 
         # represent vocabulary
         self.vocab = Integerizer(vocab)
+        self.specials_set = set() if not specials else set(specials)
+
+    @classmethod
+    def len_chunk(cls, chunk: str, specials_set: Set[str]):
+        return 1 if specials_set and chunk in specials_set else len(chunk)
+
+    def len_c(self, chunk: str):
+        return self.len_chunk(chunk, self.specials_set)
+
+    def len_p(self, packed_chunk: List[str]):
+        return sum(self.len_c(chunk) for chunk in packed_chunk)
+
+    @classmethod
+    def pack_chunks(cls, chunks: List[str], C: int, specials_set: Set[str] = None):
+        packed_chunks = list()
+        packed_chunk = list()
+        quota = C
+        for chunk in chunks:
+            length = cls.len_chunk(chunk, specials_set)
+            if length > C:
+                raise ValueError(f"{C} is not enough to pack {chunk} of length {len(chunk)}")
+            if quota >= length:
+                packed_chunk.append(chunk)
+                quota -= length
+            else:
+                packed_chunks.append(packed_chunk)
+                packed_chunk = list()
+                quota = C
+        if len(packed_chunk) > 0:
+            packed_chunks.append(packed_chunk)
+        return packed_chunks
 
     def parallel_backward_mask(self, L: int, M: int, device: str = "cpu") -> Tuple[torch.FloatTensor, torch.FloatTensor]:
         if (L, M, device) in self.parallel_backward_mask_cache:
@@ -55,7 +88,33 @@ class TokenizationMixin:
                                                                             torch.LongTensor,
                                                                             torch.FloatTensor,
                                                                             torch.FloatTensor,]:
-        L = max(len(chunk) for chunk in chunks)  # max length of chunk
+        L = max(self.len_c(chunk) for chunk in chunks)  # max length of chunk
+        return self.encode_batch_generic(chunks, L, M, self.encode_transitions, self.len_c, device=device)
+
+    def encode_packed_batch(self, packed_chunks: List[List[str]], M: int, device: str = "cpu") -> Tuple[torch.LongTensor,
+                                                                            torch.FloatTensor,
+                                                                            torch.LongTensor,
+                                                                            torch.LongTensor,
+                                                                            torch.FloatTensor,
+                                                                            torch.LongTensor,
+                                                                            torch.FloatTensor,
+                                                                            torch.FloatTensor,]:
+        L = max(self.len_p(packed_chunk) for packed_chunk in packed_chunks)  # max length of packed chunk
+        return self.encode_batch_generic(packed_chunks, L, M, self.encode_packed_transitions, self.len_p, device=device)
+
+    def encode_batch_generic(self, chunks: List[T],
+                     L: int,
+                     M: int,
+                     encoder: Callable[[List[T], int, int, ...], Tuple[torch.FloatTensor, torch.FloatTensor, torch.FloatTensor, torch.FloatTensor]],
+                     length_fn: Callable[[List[T]], int],
+                     device: str = "cpu") -> Tuple[torch.LongTensor,
+                                                    torch.FloatTensor,
+                                                    torch.LongTensor,
+                                                    torch.LongTensor,
+                                                    torch.FloatTensor,
+                                                    torch.LongTensor,
+                                                    torch.FloatTensor,
+                                                    torch.FloatTensor,]:
         B = len(chunks)
         M = min(self.max_unit_length, L, M)
 
@@ -66,12 +125,13 @@ class TokenizationMixin:
         bwd_ms = []
         lengths = []
         for chunk in chunks:
-            fwd_id, fwd_m, bwd_id, bwd_m = self.encode_transitions(chunk, L, M, device=device)
+            fwd_id, fwd_m, bwd_id, bwd_m = encoder(chunk, L, M, device=device)
             fwd_ids.append(fwd_id)
             fwd_ms.append(fwd_m)
             bwd_ids.append(bwd_id)
             bwd_ms.append(bwd_m)
-            lengths.append(len(chunk))
+            print(chunk, length_fn(chunk))
+            lengths.append(length_fn(chunk))
 
         # ts and ms are [B x max_length x max_length] tensors
         # lengths is a [B] tensor
@@ -88,49 +148,17 @@ class TokenizationMixin:
         bwd_ms = (bwd_ms.unsqueeze(1) * mmask).reshape(B * L, M, L)
         bwd_lengths = torch.repeat_interleave(lengths, L)
 
+        print(bwd_lengths)
         return fwd_ids, fwd_ms, lengths, bwd_ts, bwd_ms, bwd_lengths, mmask, emask
 
-    def encode_packed_batch(self, packed_chunks: List[List[str]], M: int, device: str = "cpu") -> Tuple[torch.LongTensor,
-                                                                            torch.FloatTensor,
-                                                                            torch.LongTensor,
-                                                                            torch.LongTensor,
-                                                                            torch.FloatTensor,
-                                                                            torch.LongTensor,
-                                                                            torch.FloatTensor,
-                                                                            torch.FloatTensor,]:
-        L = max(sum(len(chunk) for chunk in packed_chunk) for packed_chunk in packed_chunks)  # max length of chunk
-        B = len(packed_chunks)
-        M = min(self.max_unit_length, L, M)
-
-        # encode lattice as special transition matrices
-        fwd_ids = []
-        fwd_ms = []
-        bwd_ids = []
-        bwd_ms = []
-        lengths = []
-        for packed_chunk in packed_chunks:
-            fwd_id, fwd_m, bwd_id, bwd_m = self.encode_packed_transitions(packed_chunk, L, M, device=device)
-            fwd_ids.append(fwd_id)
-            fwd_ms.append(fwd_m)
-            bwd_ids.append(bwd_id)
-            bwd_ms.append(bwd_m)
-            lengths.append(sum(len(chunk) for chunk in packed_chunk))
-
-        # ts and ms are [B x max_length x max_length] tensors
-        # lengths is a [B] tensor
-        fwd_ids = torch.stack(fwd_ids)  # torch.FloatTensor
-        fwd_ms = torch.stack(fwd_ms)  # torch.FloatTensor
-        bwd_ids = torch.stack(bwd_ids)  # torch.FloatTensor
-        bwd_ms = torch.stack(bwd_ms)  # torch.FloatTensor
-        lengths = torch.tensor(lengths, dtype=torch.long,  device=device)  # torch.LongTensor
-
-        mmask, emask = self.parallel_backward_mask(L, M,  device=device)
-        mmask = mmask.unsqueeze(0)
-        emask = emask.unsqueeze(0)
-        bwd_ts = (bwd_ids.unsqueeze(1) * emask.to(torch.long)).reshape(B * L, M, L)
-        bwd_ms = (bwd_ms.unsqueeze(1) * mmask).reshape(B * L, M, L)
-        bwd_lengths = torch.repeat_interleave(lengths, L)
-        return fwd_ids, fwd_ms, lengths, bwd_ts, bwd_ms, bwd_lengths, mmask, emask
+    @staticmethod
+    def init_transitions_and_masks(M:int, L:int, device: str = None):
+        fwd_mask = torch.zeros((M, L), dtype=torch.float, device=device)  # whenever a element of a matrix is from weights, set to 1
+        bwd_mask = torch.zeros((M, L), dtype=torch.float, device=device)  # whenever a element of a matrix is from weights, set to 1
+        fwd_ids = torch.zeros((M, L), dtype=torch.int, device=device)
+        bwd_ids = torch.zeros((M, L), dtype=torch.int, device=device)
+        bwd_mask[0, :] = 1 # make sure to pad the lattice for backward
+        return fwd_ids, fwd_mask, bwd_ids, bwd_mask
 
     def encode_transitions(self, chunk: str, L: int, M: int, device="cpu") -> Tuple[torch.LongTensor, torch.FloatTensor, torch.LongTensor, torch.FloatTensor]:
         """
@@ -147,12 +175,19 @@ class TokenizationMixin:
         [ hate        ]
         then flip ^ left to right
         """
-        if len(chunk) > L:
+        if chunk not in self.specials_set and len(chunk) > L:
             raise ValueError(f"chunk length of {chunk} is greater than allowed max chunk length {L}")
-        fwd_mask = torch.zeros((M, L), dtype=torch.float, device=device)  # whenever a element of a matrix is from weights, set to 1
-        bwd_mask = torch.zeros((M, L), dtype=torch.float, device=device)  # whenever a element of a matrix is from weights, set to 1
-        fwd_ids = torch.zeros((M, L), dtype=torch.int, device=device)
-        bwd_ids = torch.zeros((M, L), dtype=torch.int, device=device)
+        fwd_ids, fwd_mask, bwd_ids, bwd_mask = self.init_transitions_and_masks(M, L, device=device)
+
+        # handle special tokens
+        if chunk in self.specials_set:
+            fwd_ids[0,0] = self.vocab.index(chunk)
+            fwd_mask[0,0] = 1
+            bwd_ids[0,0] = self.vocab.index(chunk)
+            bwd_mask[0,0] = 1
+            return fwd_ids, fwd_mask, bwd_ids, bwd_mask
+
+        # handle real tokens
         for s in range(len(chunk)):
             for l in range(min(len(chunk) - s, M) + 1):
                 unit = chunk[s:s + l]
@@ -162,8 +197,8 @@ class TokenizationMixin:
                     fwd_mask[l - 1, s + l - 1] = 1
                     fwd_ids[l - 1, s + l - 1] = self.vocab.index(unit)
                     # bwd
-                    bwd_mask[l - 1, len(chunk) - s - 1] = 1
-                    bwd_ids[l - 1, len(chunk) - s - 1] = self.vocab.index(unit)
+                    bwd_mask[l - 1, L - s - 1] = 1
+                    bwd_ids[l - 1, L - s - 1] = self.vocab.index(unit)
         return fwd_ids, fwd_mask, bwd_ids, bwd_mask
 
     def encode_packed_transitions(self, packed_chunk: List[str], L: int, M: int, device="cpu") -> Tuple[torch.LongTensor, torch.FloatTensor, torch.LongTensor, torch.FloatTensor]:
@@ -181,20 +216,19 @@ class TokenizationMixin:
         [ hate        ]
         then flip ^ left to right
         """
-        total_length = sum(len(chunk) for chunk in packed_chunk)
+        total_length = sum(self.len_c(chunk) for chunk in packed_chunk)
         if total_length > L:
             raise ValueError(f"chunk length of {packed_chunk} is greater than allowed max chunk length {L}")
-        fwd_mask = torch.zeros((M, L), dtype=torch.float, device=device)  # whenever a element of a matrix is from weights, set to 1
-        bwd_mask = torch.zeros((M, L), dtype=torch.float, device=device)  # whenever a element of a matrix is from weights, set to 1
-        fwd_ids = torch.zeros((M, L), dtype=torch.int, device=device)
-        bwd_ids = torch.zeros((M, L), dtype=torch.int, device=device)
+        fwd_ids, fwd_mask, bwd_ids, bwd_mask = self.init_transitions_and_masks(M, L, device=device)
+
         start = 0
         for chunk in packed_chunk:
-            cfi, cfm, cbi, cbm = self.encode_transitions(chunk, L=len(chunk), M=min(M, len(chunk)), device=device)
+            length = self.len_c(chunk)
+            cfi, cfm, cbi, cbm = self.encode_transitions(chunk, L=length, M=min(M,length), device=device)
             r, c = cfi.size()
             fwd_ids[:r, start:start + c] = cfi
             fwd_mask[:r, start:start + c] = cfm
-            bwd_ids[:r, total_length - start - c:total_length - start] = cbi
-            bwd_mask[:r, total_length - start - c:total_length - start] = cbm
-            start += len(chunk)
+            bwd_ids[:r, L - start - c:L - start] = cbi
+            bwd_mask[:r, L - start - c:L - start] = cbm
+            start += length
         return fwd_ids, fwd_mask, bwd_ids, bwd_mask
