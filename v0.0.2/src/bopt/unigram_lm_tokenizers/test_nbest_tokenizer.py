aa0b8a90a59cc9da5ce529bcc5df22745a990583
Brian Lu <blu@Brians-MacBook-Pro-19.local>
2023-04-13 16:35:36 -0400

    add more tests for nbest tokenizer

--- v0.0.2/src/bopt/unigram_lm_tokenizers/test_nbest_tokenizer.py
+++ v0.0.2/src/bopt/unigram_lm_tokenizers/test_nbest_tokenizer.py
@@ -51,5 +51,48 @@ def test():
 
 
 
+def test1():
+    vocabulary = Integerizer(
+        [
+            "[UNK]",
+            "h",
+            "a",
+            "t",
+            "e",
+            "hat",
+            "hate",
+            "at",
+            "ate",
+        ]
+    )
+    log_potentials = torch.tensor([math.log(2.0)] * len(vocabulary)).unsqueeze(-1)
+
+    tokenizer = NBestTokenizer(vocabulary, pretrained_log_potentials=log_potentials)
+    memoizer = OnDiskTensorMemoizer("/tmp/bopt/test_lattice_tokenizer", overwrite=True, debug=True)
+    sentence_ids = [["1-1", "1-2"],["2-1", "2-2"]]
+    output = tokenizer([["hate", "ateat"],["ate", "hat"]],
+                max_blocks = 2,
+                max_unit_length = 4,
+                max_block_length = 5,
+                space_character = " ",
+                split_on_space = True,
+                add_dummy_space_start = False,
+                remove_space=True,
+                memoizer=memoizer,
+                sentence_ids=sentence_ids,
+                use_lattice_position_ids=True,
+                n=6)
+    for i in range(2):
+        for j in range(6):
+            print(f"batch{i}, top{j}")
+            print(" ".join([vocabulary[id] for id in output.input_ids[i][j]]))
+            print(output.attention_mask[i][j])
+            print(output.position_ids[i][j])
+            print(output.type_ids[i][j])
+            print(output.weights[i,j].exp().item())
+
+
+
 if __name__ == "__main__":
     test()
+    test1()
