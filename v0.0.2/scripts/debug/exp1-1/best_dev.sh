8051ce07ed4b0607fd07a5b02721d88652b84984
Brian Lu <blu@Brians-MacBook-Pro-19.local>
2023-04-14 12:21:53 -0400

    remove stale files

--- v0.0.2/scripts/debug/exp1-1/best_dev.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-for L1 in 0.01 0.1 1.0
-do
-python3 ../../simple/analysis/best_dev.py ${BLU_ARTIFACTS}/bopt/syn3/exp34-11/42/${L1}/768/log.json eval_zero_one_loss
-python3 ../../simple/analysis/best_dev.py ${BLU_ARTIFACTS}/bopt/syn3/exp34-11/42/${L1}/768/log.json eval_expected_zero_one_loss
-python3 ../../simple/analysis/best_dev.py ${BLU_ARTIFACTS}/bopt/syn3/exp34-11/42/${L1}/768/log.json eval_log_loss
-python3 ../../simple/analysis/best_dev.py ${BLU_ARTIFACTS}/bopt/syn3/exp34-11/42/${L1}/768/log.json train_loss
-done
